1542b651c4edf1cbfba65ab23c9c8390492f98a3
Voronkov Vitalii <voronkoff.vitaly@gmail.com>
2017-09-02 13:17:46 +0300

    Added @POST, @PUT, @DELETE resources.

--- src/main/java/com/vvoronkov/messenger/resources/MessageResource.java
+++ src/main/java/com/vvoronkov/messenger/resources/MessageResource.java
@@ -3,28 +3,44 @@ package com.vvoronkov.messenger.resources;
 import com.vvoronkov.messenger.model.Message;
 import com.vvoronkov.messenger.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages(){
         return messageService.getAllMessages();
     }
 
+    @POST
+    public Message addMessage(Message message){
+        return messageService.addMessage(message);
+    }
+
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long id){
         return messageService.getMessage(id);
+    }
+
+
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long id, Message message){
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long id){
+        messageService.removeMessage(id);
     }
 }
